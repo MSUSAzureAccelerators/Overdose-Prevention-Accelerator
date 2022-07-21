ddccdbec38dfcaed4e27b09198a18b854ac17cdf
Jason Berg <jason.berg@thespurgroup.com>
2022-07-21 11:57:50 -0700

    typo

--- Overdose-Accelerator-Web/Models/WebserviceUrl.cs
+++ Overdose-Accelerator-Web/Models/WebserviceUrl.cs
@@ -4,7 +4,7 @@ namespace OverdoseAcceleratorWeb.Models
 	public class WebServiceUrl
 	{
 		public string url { get; set; }
-		public string token { get; set; }; 
+		public string token { get; set; } 
 		public WebServiceUrl() { }
 	}
 }
